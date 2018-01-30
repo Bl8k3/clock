5a826050a601ad8a6bcd2eb15a9219d2c83fb216
Bl8k3 <blake.littleton@gmail.com>
2018-01-29 22:40:14 -0500

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,18 @@
+console.log("script loaded");
+function clock(); {
+    var MINUTE = 20;
+    var PERIOD = "AM";
+    var HOUR = 8;
+
+    if (MINUTE < 30 && PERIOD="AM"); {
+        console.log("it's almost nine in the morining.");
+    };
+    var MINUTE = 15;
+    var PERIOD = "PM";
+    var HOUR = 7;
+    if (MINUTE < 30 && PERIOD=="PM"; {
+        console.log("it's just after seven in the evening");
+    };
+};
+
+
